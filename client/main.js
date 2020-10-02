44afcf486ae0e723e2e71c204f45f1c1035c8273
Bagas Gusti Pamungkas <67892953+bagasss19@users.noreply.github.com>
2020-10-02 11:42:52 +0700

    update front end

--- client/main.js
+++ client/main.js
@@ -1,144 +1,86 @@
-let baseUrl = "http://localhost:3000"
+const baseurl = "http://localhost:3000"
+
 
 $(document).ready(function () {
-  checkLogin()
+    checkLogin()
 })
 
+
 function login(event) {
-  event.preventDefault() //biar form gak refresh mulu
-  let email = $("#login-email").val()
-  let password = $("#login-password").val()
-  $.ajax({
-    url: `${baseUrl}/users/login`,
-    method: "post",
-    data: {
-      email,
-      password
-    }
-  })
+    event.preventDefault()
+    let email = $("#email").val()
+    let password = $("#password").val()
+    console.log(email,password)
+    $.ajax({
+        url : `${baseurl}/login`,
+        method : "post",
+        data : {
+            email , 
+            password
+        }
+    })
     .done(data => {
-      console.log(data, '<<<<<data login')
-      localStorage.setItem('token', data.token)
-      checkLogin() //cek function ceklogin, setelah berhasil login
+        console.log(data);
+        localStorage.setItem("token", data.token)
+        checkLogin()
     })
     .fail(err => {
-      console.log(err.responseJSON.errors, '<<<<<err login')
-      Swal.fire('error', err.responseJSON.errors.join(', '), 'error')
-      // showError(err.responseJSON.errors)
-    })
-    .always(_ => {//biar datanya kosong lagi
-      $("#login-email").val('')
-      $("#login-password").val('')
+        console.log(err.responseJSON.err);
     })
 }
 
 function checkLogin() {
-  if (localStorage.token) { //penanda user login pakai token
-    $("#home-page").show()
-    $("#login-page").hide()
-    $("#register-page").hide()
-    $("#error").hide()
-    fetchFood()
-  } else {
-    $("#home-page").hide()
-    $("#login-page").show()
-    $("#register-page").hide()
-    $("#error").hide()
-  }
-}
-
-function checkRegister() {
-  checkLogin()
-  $("#register-page").show()
-  $("#login-page").hide()
-}
-
-function register(event) {
-  event.preventDefault() //biar form gak refresh mulu
-  let email = $("#register-email").val()
-  let password = $("#register-password").val()
-  $.ajax({
-    url: `${baseUrl}/users/register`,
-    method: "post",
-    data: {
-      email,
-      password
+    if (localStorage.token) {
+        $('#homepage').show()
+        $('#login_page').hide()
+        $('#register_page').hide()
+        getData()
+    } else {
+        $('#homepage').hide()
+        $('#login_page').show()
+        $('#register_page').show()
     }
-  })
-    .done(data => {
-      console.log(data, '<<<<<data register')
-      // localStorage.setItem('token', data.token)
-      checkLogin() //cek function ceklogin, setelah berhasil register
-    })
-    .fail(err => {
-      console.log(err.responseJSON.errors, '<<<<<err register')
-      showError(err.responseJSON.errors)
-    })
-    .always(_ => {//biar datanya kosong lagi
-      $("#register-email").val('')
-      $("#register-password").val('')
-    })
-}
-
-function onSignIn(googleUser) {
-  var tokenGoogle = googleUser.getAuthResponse().id_token;
-  console.log(tokenGoogle)
-  //kirim id_token google ke backend
-  $.ajax({
-    url: baseUrl + '/users/googleSign',
-    method: 'POST',
-    data: {
-      tokenGoogle
-    }
-  })
-    .done(res => {
-      console.log(res, '<<<hasil token generate by server aslinya mah token google')
-      localStorage.setItem('token', res.token)
-      // checkLogin() //cek function ceklogin, setelah berhasil login
-    })
-    .fail(err => {
-      console.log(err)
-    })
-}
-
-function addSearchFood() {
-  let keyword = $('#search-food').val()
-  $.ajax({
-    url: `${baseUrl}/search-food/${keyword}`, //router.get('/search-food/:keyword', searchFood)
-    method: 'get',
-    headers: {
-      token: localStorage.token
-    }
-  })
-    .done(data => {
-      console.log(data.restaurants, '<<<<<data todo')
-      $('#container-search').empty()
-      data.restaurants.forEach(el => {
-        $('#container-search').append(`
-        <li>
-          <label>Restaurant Name : ${el.name}</label><br>
-          <label>Link : ${el.url}</label><br>
-          <label>Address : ${el.location.address}</label><br>
-          <label>Photos : ${el.images}</label><br>
-          <label>Menus : ${el.menus}</label><br>
-          <label>Weather : ${el.weather.description}</label><br>
-          <label>Logo : ${el.weather.imageUrl}</label><br>
-        </li><br>
-        `)
-      });
-    })
-    .fail(err => {
-      console.log(err.responseJSON.errors, '<<<<<err data todo')
-      showError(err.responseJSON.errors)
-    })
 }
 
 function logout() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
+    localStorage.clear()
+    checkLogin()
+}
 
-  localStorage.clear()
-  checkLogin()
+//buat todonya
+$(document).ready(function () {
+    $('#add-todo').click(function () {
+        $('#todo').append("<li>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></li>");
+    });
+    $("body").on('click', '#todo a', function () {
+        $(this).closest("li").remove();
+    });
+});
+
+function getData() {
+    $.ajax({
+        url : `${baseurl}/todos`,
+        method : "get",
+        headers : {
+            token : localStorage.token
+        }
+    })
+    .done(data =>{
+        console.log(data.data);
+        data.data.forEach(x => {
+            $('#todo').append(`
+            <tr>
+            <td style="font-family: 'Tangerine', serif; font-size:35px;">${x.title}</td>
+            <td style="font-family: 'Tangerine', serif; font-size:35px;">${x.description}</td>
+            <td style="font-family: 'Tangerine', serif; font-size:35px;">
+            <button>Done</button>
+            <button>Edit</button>
+            <a href='#'><button>Delete</button></a>
+            </td>
+            </tr>`)
+        })
+    })
+    .fail(err => {
+        console.log(err);
+    })
 }
